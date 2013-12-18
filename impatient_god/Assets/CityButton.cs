c53d4fa272a28265b48bc9eecd94cc20ecb5559c
Matt Blair <blair1618@gmail.com>
2013-12-18 01:51:53 -0800

    City folk now die after some time interval

--- impatient_god/Assets/CityButton.cs
+++ impatient_god/Assets/CityButton.cs
@@ -3,33 +3,51 @@ using System.Collections;
 
 public class CityButton : MonoBehaviour {
 
+	public static System.TimeSpan deathInterval = System.TimeSpan.FromSeconds(7);
+	public static Color deadColor = Color.gray;
+
 	public FoodBarController foodBar;
 	public GameObject cityFolkPrefab;
 
-	Bounds cityBounds;
+	System.DateTime lastDeathTime;
+	System.DateTime nextDeathTime;
+
+	Vector2 cityBounds;
 	GameObject[] folks;
 	int nFolks = 0;
 
 	// Use this for initialization
 	void Start () {
-		cityBounds = GetComponent<SpriteRenderer> ().sprite.bounds;
-		print (cityBounds);
+		cityBounds = GetComponent<BoxCollider2D> ().size;
 		folks = new GameObject[64];
+		lastDeathTime = System.DateTime.Now;
+		nextDeathTime = lastDeathTime.Add (deathInterval);
+		//TODO: Randomize death Interval
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+		if (System.DateTime.Now.CompareTo (nextDeathTime) > 0) {
+			//Kill a folk!
+			foreach (GameObject folk in folks) {
+				if (folk.GetComponent<SpriteRenderer>().color != deadColor) {
+					folk.GetComponent<SpriteRenderer>().color = deadColor;
+					break;
+				}
+			}
+			lastDeathTime = System.DateTime.Now;
+			nextDeathTime = lastDeathTime.Add (deathInterval);
+		}
 	}
 
 	void OnMouseUp () {
 		print ("clicked!");
 		if (foodBar.useFood ()) {
-			print ("New citizen!");
-			GameObject newFolk = (GameObject)Instantiate(cityFolkPrefab);
-			float newX = Random.Range(cityBounds.min.x, cityBounds.max.x);
-			float newY = Random.Range(cityBounds.min.y, cityBounds.max.y);
-			newFolk.transform.Translate(newX, newY, 0);
+			print ("New folk!");
+			Vector3 newPosition = new Vector3(Random.Range(-cityBounds.x/2, cityBounds.x/2), 
+			                                  Random.Range(-cityBounds.y/2, cityBounds.y/2), 
+			                                  transform.position.z);
+			GameObject newFolk = (GameObject)Instantiate(cityFolkPrefab, newPosition, Quaternion.identity);
 			print (newFolk.transform.position);
 			folks[nFolks] = newFolk;
 			nFolks++;
