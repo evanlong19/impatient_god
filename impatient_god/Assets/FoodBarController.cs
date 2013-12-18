9e9cc3ddfdf881dbd9c477eab145fdbd09fc065c
Matt Blair <blair1618@gmail.com>
2013-12-17 18:54:33 -0800

    changes to foodBarController, person placeholder

--- impatient_god/Assets/FoodBarController.cs
+++ impatient_god/Assets/FoodBarController.cs
@@ -3,31 +3,15 @@ using System.Collections;
 
 public class FoodBarController : MonoBehaviour {
 
-	int foodCount = 5;
+	int foodCount = 4;
 
-	public Texture2D food0of5tex;
-	public Texture2D food1of5tex;
-	public Texture2D food2of5tex;
-	public Texture2D food3of5tex;
-	public Texture2D food4of5tex;
-	public Texture2D food5of5tex;
-	
-	Texture2D[] texs = new Texture2D[6];
+	public Sprite[] sprites = new Sprite[6];
 
 	public SpriteRenderer render;
 
-	Sprite currentSprite;
-
 	// Use this for initialization
 	void Start () {
-		texs [0] = food0of5tex;
-		texs [1] = food1of5tex;
-		texs [2] = food2of5tex;
-		texs [3] = food3of5tex;
-		texs [4] = food4of5tex;
-		texs [5] = food5of5tex;
-
-		currentSprite = render.sprite;
+		render.sprite = sprites[foodCount];
 	}
 	
 	// Update is called once per frame
@@ -37,8 +21,8 @@ public class FoodBarController : MonoBehaviour {
 
 	public int useFood() {
 		foodCount = Mathf.Max (0, --foodCount);
-		if (foodCount < texs.GetLength (0)) {
-			currentSprite.texture = texs[foodCount];
+		if (foodCount < sprites.GetLength (0)) {
+			render.sprite = sprites[foodCount];
 		}
 		return foodCount;
 	}
