ff180389b54206ccd8441d6dbd7d0b5c26a7f881
Matt Blair <blair1618@gmail.com>
2013-12-17 20:12:01 -0800

    Add new images to scene, add randomly placed people on city click, add foodBar regeneration

--- impatient_god/Assets/CityButton.cs
+++ impatient_god/Assets/CityButton.cs
@@ -4,14 +4,19 @@ using System.Collections;
 public class CityButton : MonoBehaviour {
 
 	public FoodBarController foodBar;
+	public GameObject cityFolkPrefab;
 
 	Bounds cityBounds;
+	GameObject[] folks;
+	int nFolks = 0;
 
 	// Use this for initialization
 	void Start () {
 		cityBounds = GetComponent<SpriteRenderer> ().sprite.bounds;
+		print (cityBounds);
+		folks = new GameObject[64];
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
@@ -19,8 +24,15 @@ public class CityButton : MonoBehaviour {
 
 	void OnMouseUp () {
 		print ("clicked!");
-		if (foodBar.useFood () > 0) {
+		if (foodBar.useFood ()) {
 			print ("New citizen!");
+			GameObject newFolk = (GameObject)Instantiate(cityFolkPrefab);
+			float newX = Random.Range(cityBounds.min.x, cityBounds.max.x);
+			float newY = Random.Range(cityBounds.min.y, cityBounds.max.y);
+			newFolk.transform.Translate(newX, newY, 0);
+			print (newFolk.transform.position);
+			folks[nFolks] = newFolk;
+			nFolks++;
 		} else {
 			print ("Not enough food!");
 		}
--- impatient_god/Assets/FoodBarController.cs
+++ impatient_god/Assets/FoodBarController.cs
@@ -3,27 +3,41 @@ using System.Collections;
 
 public class FoodBarController : MonoBehaviour {
 
-	int foodCount = 4;
+	public static int maxFood = 5;
+	public static System.TimeSpan foodInterval = System.TimeSpan.FromSeconds(5);
 
-	public Sprite[] sprites = new Sprite[6];
+	int foodCount = 5;
+	System.DateTime lastFoodTime;
+	System.DateTime nextFoodTime;
+
+	public Sprite[] sprites = new Sprite[maxFood + 1];
 
 	public SpriteRenderer render;
 
 	// Use this for initialization
 	void Start () {
 		render.sprite = sprites[foodCount];
+		lastFoodTime = System.DateTime.Now;
+		nextFoodTime = lastFoodTime.Add (foodInterval);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (System.DateTime.Now.CompareTo (nextFoodTime) > 0) {
+			//Add food!
+			foodCount = Mathf.Min(foodCount + 1, maxFood);
+			render.sprite = sprites[foodCount];
+			lastFoodTime = System.DateTime.Now;
+			nextFoodTime = lastFoodTime.Add (foodInterval);
+		}
 	}
 
-	public int useFood() {
+	public bool useFood() {
+		bool hasFood = foodCount > 0;
 		foodCount = Mathf.Max (0, --foodCount);
-		if (foodCount < sprites.GetLength (0)) {
+		if (foodCount <= maxFood) {
 			render.sprite = sprites[foodCount];
 		}
-		return foodCount;
+		return hasFood;
 	}
 }
