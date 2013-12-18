1d6c966651b18ed6f9efe8080cbac2ff6edf36d5
Matt Blair <blair1618@gmail.com>
2013-12-17 17:52:51 -0800

    Simple interaction iwth placeholder city button and foodbar

---
+++ impatient_god/Assets/FoodBarController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodBarController : MonoBehaviour {
+
+	int foodCount = 4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int useFood() {
+		foodCount = Mathf.Max (0, --foodCount);
+		return foodCount;
+	}
+}
